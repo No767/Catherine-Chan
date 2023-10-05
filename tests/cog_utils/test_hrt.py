5ff4b7a1344533f11413cd09e01e8d37a1a8d2f5
No767 <73260931+No767@users.noreply.github.com>
2023-10-05 02:13:04 -0700

    Do not perform equality checks for floats
    one of the most classic CS problems that caused agony for an AP CSA assignment

--- tests/cog_utils/test_hrt.py
+++ tests/cog_utils/test_hrt.py
@@ -1,4 +1,5 @@
 import sys
+from math import isclose
 from pathlib import Path
 
 another_path = Path(__file__).parents[2].joinpath("bot")
@@ -23,23 +24,33 @@ def test_calc_e_poml():
     assert res["pmol_l_output"] == 0 and res["pg_ml_output"] == 0.544775997234935
 
     second_res = calc_e(AMOUNT, E_CONSTANT, PG_ML)
-    assert second_res["pmol_l_output"] == 7.3424674 and second_res["pg_ml_output"] == 0
+    assert (
+        isclose(second_res["pmol_l_output"], 7.3424674)
+        and second_res["pg_ml_output"] == 0
+    )
 
 
 def test_calc_prog():
     res = calc_prog_or_t(AMOUNT, PROG_CONSTANT, NMOL_L)
-    assert res["nmol_l_output"] == 0.6289308176100629 and res["ng_dl_output"] == 0
+    assert (
+        isclose(res["nmol_l_output"], 0.6289308176100629) and res["ng_dl_output"] == 0
+    )
 
     second_res = calc_prog_or_t(AMOUNT, PROG_CONSTANT, NG_DL)
-    assert second_res["ng_dl_output"] == 6.36 and second_res["nmol_l_output"] == 0
+    assert (
+        isclose(second_res["ng_dl_output"], 6.36) and second_res["nmol_l_output"] == 0
+    )
 
 
 def test_calc_t():
     res = calc_prog_or_t(AMOUNT, T_CONSTANT, NMOL_L)
-    assert res["nmol_l_output"] == 57.68626172833809 and res["ng_dl_output"] == 0
+    assert isclose(res["nmol_l_output"], 57.68626172833809) and res["ng_dl_output"] == 0
 
     second_res = calc_prog_or_t(AMOUNT, T_CONSTANT, NG_DL)
-    assert second_res["ng_dl_output"] == 0.0693406 and second_res["nmol_l_output"] == 0
+    assert (
+        isclose(second_res["ng_dl_output"], 0.0693406)
+        and second_res["nmol_l_output"] == 0
+    )
 
 
 def test_build_hrt_embed():
