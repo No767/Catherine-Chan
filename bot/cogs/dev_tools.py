629bd9fabd21d4bc842771f149f43304da150e29
No767 <73260931+No767@users.noreply.github.com>
2023-10-14 18:38:25 -0700

    Fix unused imports

--- bot/cogs/dev_tools.py
+++ bot/cogs/dev_tools.py
@@ -3,7 +3,6 @@ from typing import Literal, Optional
 import discord
 from catherinecore import Catherine
 from cogs import EXTENSIONS
-from discord import app_commands
 from discord.ext import commands
 from discord.ext.commands import Context, Greedy
 
