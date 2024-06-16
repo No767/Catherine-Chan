99f52a67a946257f6f5e37439f702ea726d3ef6a
No767 <73260931+No767@users.noreply.github.com>
2024-06-16 00:30:11 -0700

    Adjust migrations to work with current prod data

--- bot/migrations/V1__initial_migrations.sql
+++ bot/migrations/V1__initial_migrations.sql
@@ -3,10 +3,6 @@
 -- Creation Date: 2024-05-20 19:06:40.005347 UTC
 -- Reason: initial_migrations
 
--- Remove tonetags feature from the database
-DROP TABLE IF EXISTS tonetags CASCADE;
-DROP TABLE IF EXISTS tonetags_lookup CASCADE;
-
 CREATE TABLE IF NOT EXISTS profiles (
     id SERIAL PRIMARY KEY,
     views INT DEFAULT 0,
@@ -37,15 +33,3 @@ CREATE TABLE IF NOT EXISTS pronouns_examples (
 );
 
 CREATE INDEX IF NOT EXISTS pronouns_examples_user_idx ON pronouns_examples (user_id);
-
-DROP TABLE IF EXISTS blacklist;
-
-CREATE TABLE IF NOT EXISTS blacklist (
-    id SERIAL PRIMARY KEY,
-    guild_id BIGINT,
-    entity_id BIGINT,
-    UNIQUE (entity_id)
-);
-
-CREATE INDEX IF NOT EXISTS blacklist_guild_id_idx ON blacklist (guild_id);
-CREATE INDEX IF NOT EXISTS blacklist_entity_id_idx ON blacklist (entity_id);
