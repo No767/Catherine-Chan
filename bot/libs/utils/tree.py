b23a08634a039fff876e7bb25361f1d98bec9f5b
No767 <73260931+No767@users.noreply.github.com>
2024-06-29 01:03:43 -0700

    Give different response for blacklisted for guilds and users

--- bot/libs/utils/tree.py
+++ bot/libs/utils/tree.py
@@ -58,16 +58,6 @@ def _build_missing_perm_embed(
 # At the very least better than Jade's on_interaction checks
 # https://github.com/LilbabxJJ-1/PrideBot/blob/master/main.py#L19-L36
 class CatherineCommandTree(CommandTree):
-    async def _send_blacklist_response(self, interaction: discord.Interaction) -> None:
-        bot: Catherine = interaction.client  # type: ignore
-        bot.metrics.blacklist.commands.inc(1)
-        msg = (
-            f"My fellow user, {interaction.user.mention}, you just got the L. "
-            "You are blacklisted from using this bot. Take an \U0001f1f1, \U0001f1f1oser. "
-            "[Here is your appeal form](https://media.tenor.com/K9R9beOgPR4AAAAC/fortnite-thanos.gif)"
-        )
-        await interaction.response.send_message(msg, ephemeral=True)
-
     async def interaction_check(self, interaction: discord.Interaction) -> bool:
         bot: Catherine = interaction.client  # type: ignore # Pretty much returns the subclass anyways. I checked - Noelle
 
@@ -78,11 +68,21 @@ class CatherineCommandTree(CommandTree):
             return True
 
         if interaction.user.id in bot.blacklist:
-            await self._send_blacklist_response(interaction)
+            bot.metrics.blacklist.commands.inc(1)
+            msg = (
+                f"My fellow user, {interaction.user.mention}, you just got the L. "
+                "You are blacklisted from using this bot. Take an \U0001f1f1, \U0001f1f1oser. "
+                "[Here is your appeal form](https://media.tenor.com/K9R9beOgPR4AAAAC/fortnite-thanos.gif)"
+            )
+            await interaction.response.send_message(msg, ephemeral=True)
             return False
 
         if interaction.guild and interaction.guild.id in bot.blacklist:
-            await self._send_blacklist_response(interaction)
+            bot.metrics.blacklist.commands.inc(1)
+            await interaction.response.send_message(
+                "This is so sad lolllllll! Your whole entire server got blacklisted!",
+                ephemeral=True,
+            )
             return False
 
         bot.metrics.commands.invocation.inc()
