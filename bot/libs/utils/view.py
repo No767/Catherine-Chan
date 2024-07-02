be80ee333c88022b1f74de64ffa4bd39800f56fb
No767 <73260931+No767@users.noreply.github.com>
2024-07-01 21:09:37 -0700

    Include logging view and modal errors into stdlib

--- bot/libs/utils/view.py
+++ bot/libs/utils/view.py
@@ -1,9 +1,14 @@
+from __future__ import annotations
+
 import asyncio
-from typing import Any, Optional
+from typing import TYPE_CHECKING, Any, Optional
 
 import discord
 
 from .embeds import FullErrorEmbed, TimeoutEmbed
+
+if TYPE_CHECKING:
+    from catherinecore import Catherine
 
 NO_CONTROL_MSG = "This view cannot be controlled by you, sorry!"
 
@@ -19,6 +24,7 @@ class CatherineView(discord.ui.View):
         self.interaction = interaction
         self.original_response: Optional[discord.InteractionMessage]
         self.triggered = asyncio.Event()
+        self.bot: Catherine = interaction.client  # type: ignore
 
     async def interaction_check(self, interaction: discord.Interaction, /) -> bool:
         if interaction.user and interaction.user.id in (
@@ -42,6 +48,9 @@ class CatherineView(discord.ui.View):
         item: discord.ui.Item[Any],
         /,
     ) -> None:
+        self.bot.logger.exception(
+            "Ignoring view exception from %s: ", self.__class__.__name__, exc_info=error
+        )
         await interaction.response.send_message(
             embed=FullErrorEmbed(error), ephemeral=True
         )
