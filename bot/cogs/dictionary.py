d10ac16c9bdf7f42c7f4fdf9643e7e5c71db757e
No767 <73260931+No767@users.noreply.github.com>
2024-10-27 14:31:52 -0700

    fix

--- bot/cogs/dictionary.py
+++ bot/cogs/dictionary.py
@@ -233,13 +233,11 @@ class Dictionary(commands.GroupCog, name="dictionary"):
                 # Special case here
                 if "perseus.tufts.edu" in entity:
                     keyword = entity.split("=")[-1]
-                    reference_url = URL(entity[:-len(keyword) + 1])
-                    updated_ref = reference_url.with_query({"doc": reference_url.query["doc"].replace(")", "%29").replace("(", "%28")})
-                    replacements.update(
-                        {
-                            entity: f"[{keyword}]({updated_ref})"
-                        }
+                    reference_url = URL(entity[: -len(keyword) + 1])
+                    updated_ref = reference_url.with_query(
+                        {"doc": reference_url.query["doc"].replace(")", "%29").replace("(", "%28")}
                     )
+                    replacements.update({entity: f"[{keyword}]({updated_ref})"})
                     continue
 
                 link_parts = entity.partition("=")
