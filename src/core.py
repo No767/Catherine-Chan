19b7eb139c542e71d167910a16b672dcb6570164
No767 <73260931+No767@users.noreply.github.com>
2025-12-21 01:18:13 -0800

    make adjustments to work for systemd

--- src/core.py
+++ src/core.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
 import asyncio
+import gzip
 import importlib
 import logging
 import os
+import shutil
 import sys
 import uuid
 from logging.handlers import RotatingFileHandler
@@ -223,8 +225,47 @@ class Blacklist[T]:
         return self._db
 
 
+class GzipRotatingFileHandler(RotatingFileHandler):
+    def __init__(self, *, max_bytes: int, backup_count: int):
+        self.rotator = self._rotator
+        self.namer = self._namer
+
+        super().__init__(
+            filename=self._determine_filename(),
+            encoding="utf-8",
+            mode="w",
+            maxBytes=max_bytes,
+            backupCount=backup_count,
+        )
+
+    def _determine_filename(self) -> Path:
+        try:
+            # Checks for whether systemd's log directory is set. If set, send the logs to /var/lib/catherine
+            logs_dir = Path(os.environ["LOGS_DIRECTORY"]) / "catherine.log"
+            return logs_dir.resolve()
+
+        except KeyError:
+            logs_parent_dir = Path("logs/catherine.log").parent
+
+            if not logs_parent_dir.exists():
+                logs_parent_dir.mkdir()
+
+            final_logs_dir = logs_parent_dir / "catherine.log"
+            return final_logs_dir.resolve()
+
+    def _namer(self, name: str) -> str:
+        return name + ".gz"
+
+    def _rotator(self, source: str, dest: str) -> None:
+        with Path.open(source, mode="rb") as f_source, gzip.open(dest, "wb") as f_dest:
+            shutil.copyfileobj(f_source, f_dest)
+
+        Path.unlink(source)
+
+
 class CatherineLogger:
     MAX_BYTES = 32 * 1024 * 1024  # 32 MiB
+    BACKUP_COUNT = 5
 
     def __init__(self) -> None:
         self._disable_watchfiles_logger()
@@ -257,12 +298,9 @@ class CatherineLogger:
         handler.setFormatter(self._get_formatter())
 
         if not self._is_docker():
-            file_handler = RotatingFileHandler(
-                filename="catherine.log",
-                encoding="utf-8",
-                mode="w",
-                maxBytes=self.MAX_BYTES,
-                backupCount=5,
+            file_handler = GzipRotatingFileHandler(
+                max_bytes=self.MAX_BYTES,
+                backup_count=self.BACKUP_COUNT,
             )
             file_handler.setFormatter(self._get_formatter())
 
