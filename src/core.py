ebb5aa08fa3b9779cf492cf668be7ee5ac5806b7
No767 <73260931+No767@users.noreply.github.com>
2025-12-24 00:03:37 -0800

    done

--- src/core.py
+++ src/core.py
@@ -491,9 +491,7 @@ class Catherine(commands.Bot):
     async def on_command_error(
         self, ctx: commands.Context, error: commands.CommandError
     ) -> None:
-        if not self._dev_mode:
-            self.logger.exception("Ignoring exception:", exc_info=error)
-            return
+        return
 
     async def setup_hook(self) -> None:
         self.add_view(ApprovePronounsExampleView(self, "", 0, 10))
